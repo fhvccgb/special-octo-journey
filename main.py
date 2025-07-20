6f5f8d9ede78ae165dd45de30677778c99336309
fhvccgb <graunkehenry@gmail.com>
2025-07-20 11:02:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -424,7 +424,12 @@ def unban_user(user_id):
         flash('User unbanned!', 'success')
     
     return redirect(url_for('moderate_forum'))
-
+@app.route('/admin')
+def admin():
+    return render_template('admin.html',
+                           is_admin=requires_admin(),
+                           is_logged_in=requires_login(),
+                           current_user=get_current_user()
 @app.route('/admin_panel', methods=['GET', 'HEAD'])
 def admin_panel():
     """Main admin panel"""
